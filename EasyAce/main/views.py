eaac28e620ad746ce1706d5b1ed660919d94197b
LiuChang <lc3018121@sjtu.edu.cn>
2016-07-06 14:50:36 +0800

    information tutor & student

--- EasyAce/main/views.py
+++ EasyAce/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from myAuth.models import Tutor
+from myAuth.models import Tutor,MyUser,Student
 # Create your views here.
 
 def index(request):
@@ -9,9 +9,18 @@ def information_tutor(request,id):
     print(tutors)
     return render(request, 'information_tutor.html', {'tutors':tutors})
 def information(request,id):
-    tutors = Tutor.objects.filter(id=id)
-    print(tutors)
-    return render(request, 'information_tutor.html', {'tutors':tutors})
+    try:
+        user = MyUser.objects.get(pk=id)
+    except DoseNotExist as e:
+        pass
+    if user.role=='tutor':
+        tutor = user.get_user()
+        return render(request, 'information_tutor.html', {'tutor':tutor})
+    elif user.role=='student':
+        student = user.get_user()
+        return render(request, 'information_student.html', {'student':student})
+    else:
+        return render(request, 'index.html')
 def view_tutor(request):
     param1 = request.GET.get('p1')
     param2 = request.GET.get('p2')
