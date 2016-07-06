0f061413b0bf59b15474dd958c00603a6f68125f
LiuChang <lc3018121@sjtu.edu.cn>
2016-07-06 14:31:20 +0800

    test for Models in auth

---
+++ EasyAce/main/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+app_name = 'main'
+
+urlpatterns = [
+    url(r'^index/$', views.index),
+    url(r'^$', views.index),
+    url(r'^tutors/',views.view_tutor),
+    url(r'information/(?P<id>[0-9]+)',views.information,name='information'),
+]
