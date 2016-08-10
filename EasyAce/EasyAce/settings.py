6f0679ad6d315bbfb0ea36ab34b4cf69187ff159
kangarootao <kangarootao@kangarootaodemacbook-pro.local>
2016-08-10 22:09:15 +0800

    push clipboard

--- EasyAce/EasyAce/settings.py
+++ EasyAce/EasyAce/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_URL = './static/'
+#STATIC_ROOT = './static/'
 MEDIA_ROOT = 'photos/'
 MEDIA_URL = 'photos/'
 
